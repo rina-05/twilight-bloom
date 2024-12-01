75fe378ebfbb81237695381127dec715e931a22a
rina-05 <rk69995@ubt-uni.net>
2024-12-01 18:52:59 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 //Menu Open Close
-let menu = document.querySelector('.menu-icon')
+let menu = document.querySelector(".menu-icon")
 
 menu.onclick = () => {
-    menu.classList.toggle('move')
-}
+    menu.classList.toggle("move")
+};
+//swiper
+var swiper = new Swiper(".services-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 50000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
