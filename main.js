8b70be5500ca2e489687d975d2a1046d670b231e
rina-05 <rk69995@ubt-uni.net>
2024-12-03 19:56:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+//Menu Open Close
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+
+};
+//swiper
+var swiper = new Swiper(".services-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 50000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+//header
+let header = document.querySelector("header");
+
+window.addEventListener("scroll",() => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+})
+
+
+
+
