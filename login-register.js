2606d93b5465967cc41ff5b9bc881a2b2e8da621
rina-05 <rk69995@ubt-uni.net>
2024-12-03 19:57:00 +0100

    Add files via upload

---
+++ login-register.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    document.getElementById('submit').addEventListener('click', function(event) {
+        
+        event.preventDefault();
+
+        //input e,mail and pass
+        const email = document.querySelector('.input-field[type="text"]').value;
+        const password = document.querySelector('.input-field[type="password"]').value;
+
+        // valid
+        if (email && password) {
+
+            //main pg
+            window.location.href = "index.html";
+        } else {
+            //no email or pass
+            alert("Please enter both email and password.");
+        }
+    });
+});
