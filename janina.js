428f9a358d7a9b8817ad41293c92bdf0f8af6359
janina-05 <jg70084@ubt-uni.net>
2024-12-06 13:41:47 +0100

    janina-commit

--- janina.js
+++ janina.js
@@ -1,14 +1,1 @@
-var swiper = new Swiper(".mySwiper", {
- spaceBetween: 30,
- centeredSlides: true,
- autoplay: {
-    delay:50000,
-    disableOnInteraction: false,
-
- },
- navigation: {
-    nextE1: ".swiper-button-next",
-    prevE1: ".swiper-button-prev",
- },
-});
 
