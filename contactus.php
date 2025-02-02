53520648f99de4f924435b7b9a0f83f6af0c68d0
janina-05 <jg70084@ubt-uni.net>
2025-02-02 20:52:29 +0100

    janina-commit

--- contactus.php
+++ contactus.php
@@ -6,6 +6,95 @@
   <link rel="stylesheet" href="contactus.css">
 </head>
 <body>
+
+<?php
+
+require_once 'vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+require 'PHPMailer/Exception.php';
+
+$mail = new PHPMailer(true); 
+
+
+
+try {
+  
+  $mail->isSMTP();
+  $mail->Host = 'smtp.gmail.com'; 
+  $mail->SMTPAuth = true;
+  $mail->Username = 'gjonbalajjanina@gmail.com'; 
+$mail->Password = 'jfhr pbii yevr dbzr'; 
+  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+  $mail->Port = 587;
+
+  $mail->setFrom('emaili_yt@gmail.com', 'Twilight-Bloom');
+  $mail->addAddress($_POST['email']); 
+  $mail->Subject = 'We have received your message.';
+  $mail->Body = 'Thank you for contacting us. We will get back to you as soon as possible. :)';
+
+  
+  $mail->send();
+  echo '  Emaili u dërgua me sukses!';
+} catch (Exception $e) {
+  echo " Emaili dështoi: {$mail->ErrorInfo}";
+}
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    if (empty($name) || empty($email) || empty($message)) {
+        echo "<p style='color: red;'> Të gjitha fushat janë të detyrueshme!</p>";
+    } else {
+        
+        $to = "gjonbalajjanina@gmail.com"; 
+        $subject = "Mesazh i ri nga formulari i kontaktit";
+        $body = "Emri: $name\nEmail: $email\nTelefoni: $phone\n\nMesazhi:\n$message";
+        $headers = "From: $email";
+
+        if (@mail($to, $subject, $body, $headers)) {
+            echo "<p style='color: green;'> Mesazhi u dërgua me sukses!</p>";
+        } else {
+        }
+
+        
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "contact_form";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("<p style='color: red;'>Lidhja me databazën dështoi: " . $conn->connect_error . "</p>");
+        }
+
+        $sql = "INSERT INTO messages (name, email, phone, message) 
+        VALUES ('Emri', 'email@example.com', '1234567890', '')";
+
+        $stmt = $conn->prepare("INSERT INTO messages (name, email, phone, message) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $name, $email, $phone, $message);
+
+        if ($stmt->execute()) {
+            echo "<p style='color: green;'> Mesazhi u ruajt në databazë!</p>";
+        } else {
+            echo "<p style='color: red;'> Gabim gjatë ruajtjes në databazë: " . $stmt->error . "</p>";
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+}
+?>
   <div class="container">
     <div  class="form">
       <div class="contact-info"></div>
